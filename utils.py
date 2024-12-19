ccf58e1a322c2731de905214f71944ffd9031b5b
alfredofdlv <alfredo.florez04@csioviedo.com>
2024-12-19 23:10:38 +0000

    Starting with EDA

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 def assign_station(month): 
     if month in [12,1,2]: 
         return 'Winter'
@@ -8,7 +8,7 @@ def assign_station(month):
         return 'Summer'
     elif month in [9,10,11]:
         return 'Autumn'
-def treat_date_cols(df,dc,nc):
+def treat_date_cols(df_original,dc,nc,cc):
     '''
 
     Parameters
@@ -22,17 +22,42 @@ def treat_date_cols(df,dc,nc):
     df_2 : pandas.DataFrame
 
     '''
+    df=df_original
+    #We save the latest date of the dataset for future calculations 
+    latest_date=max(df['YearRemodAdd'])
 
     for col in dc: 
-        nc.remove(col)
-        latest_date=max(df['YearRemodAdd'])
+        if col  in nc: 
+            nc.remove(col)          # We remove from the numerical columns list each one of the date columns. As we are not going to treat them as numerical.
+
+        else: 
+            print(f"Column '{col}' not found in numeric_cols.")
         if col == 'MoSold': 
-            # df['Station_Sold']=df[col].apply(assign_station)
-            # df['Station_Sold']=df['Station_Sold'].astype('category')
+            df['Station_Sold']=df[col].apply(assign_station)
+            df['Station_Sold']=df['Station_Sold'].astype('category')
+            cc.append('Station_Sold')
             df[col]=df[col].astype('category')
+            cc.append(col)
+
         if col == 'YearBuilt': 
+            # We calculate Anitquity of the House by subsctracting from the latest date possible the date of the house.
             df['House_Age']=latest_date-df[col]
-            df=df.drop(columns=col)
         if col == 'YearRemodAdd':
-            df['Year_Remod']=latest_date-df[col]
-            df=df.drop(columns=col)
+            # We calculate number of years since last remodelation
+            df['Year_Remod']=latest_date-df[col] 
+            # We create a categoric feature to see if the House has been remodeled
+            df['WasRemod']  =(df['YearBuilt'] != df['YearRemodAdd']).astype(int)
+        if col == 'GarageYrBlt' : 
+            df['Garage_Age'] = np.where(
+                                df[col].isna(),           
+                                    -1,                                 
+                                    latest_date - df[col]     
+                                        )
+            df['HasGarage'] = df[col].notnull().astype(int)
+        if col =='YrSold' : 
+            df[col]=df[col].astype('category')
+            cc.append(col)
+            # df=df.drop(columns=col)
+    print(type(df))
+    return df,dc,nc,cc
+            
